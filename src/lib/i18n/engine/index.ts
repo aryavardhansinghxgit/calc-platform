e58c80da45ae33745adc9dde22d2a0f7c8a4c632
samantvardhansingh-design <samantvardhansingh@gmail.com>
2026-09-18 14:31:34 +0530

    refactor: archive localization engine and add calculator components

--- src/lib/i18n/engine/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./types";
-export * from "./discovery";
-export * from "./formatter";
-export * from "./terminology";
-export * from "./auditor";
-export * from "./state-machine";
-export * from "./orchestrator";
